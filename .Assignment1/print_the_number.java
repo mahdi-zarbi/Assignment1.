9fb7abb32bf7a8164f3a60387829fada486c5412
mahdi <zarbi.mahdi0101@gmail.com>
2025-03-07 15:56:50 +0330

    Update print_the_number.java

--- .Assignment1/print_the_number.java
+++ .Assignment1/print_the_number.java
@@ -1,5 +1,5 @@
 public class print_the_number{
-    public static void main() {
+    public static void main(string [] args) {
         for(int i=1;i<=100;i++){
             System.out.println(i);
         }
