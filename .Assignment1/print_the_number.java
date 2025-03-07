dd90edfe871fc942d917ed4efd61674d89467a7f
mahdi <zarbi.mahdi0101@gmail.com>
2025-03-07 23:02:39 +0330

    prime-number.java

--- .Assignment1/print_the_number.java
+++ .Assignment1/print_the_number.java
@@ -1,7 +1,15 @@
-public class print_the_number{
-    public static void main(string [] args) {
-        for(int i=1;i<=100;i++){
-            System.out.println(i);
+public class prime-number{
+    public static void main(String[] args) throws Exception {
+        for(int i=2; i<=100; i++){
+            boolean number_is_Prime = true;
+            for(int j=2; j<i; j++){
+                if(i%j == 0){
+                    number_is_Prime = false;
+                }
+            }
+            if(isPrime == true){
+                System.out.println(i+"is a prime number");
+            }
         }
     }
 }
